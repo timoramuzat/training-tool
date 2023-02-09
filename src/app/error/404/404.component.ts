13f652c90ccee6abad06717a959708e1651154b4
timoramuzat <tdj.ramuzat@student.han.nl>
2023-02-09 17:57:38 +0100

    commit first

---
+++ src/app/error/404/404.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notfound-error',
+  templateUrl: './404.component.html',
+  styleUrls: ['./404.component.css']
+})
+export class NotfoundErrorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
