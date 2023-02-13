12a33640f8e69932f8f6dc90beb25647eb10340b
Timo <70514146+timoramuzat@users.noreply.github.com>
2023-02-13 23:24:10 +0100

    commit firstfsdf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const app = express();
-const port = process.env.PORT
+const port = 3000;
 
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
