02b59342d00e5c13e272225e669978c2cabc68ad
timoramuzat <tdj.ramuzat@student.han.nl>
2023-02-13 21:12:47 +0100

    commit firstfsdf

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const app = express();
-const port = process.env.FRONTEND_PORT
+const port = process.env.PORT
 
 app.use(express.static(__dirname + '/dist'));
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
