7f3ab86a04a4c14b038fb858c0b3ae94819e36be
timoramuzat <tdj.ramuzat@student.han.nl>
2023-02-13 13:03:55 +0100

    commit firstfsdf

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 var cors = require('cors')
 // var bodyParser = require('body-parser')
-const port = 3000;
+const PORT = process.env.PORT || 5000
 
 
 // all routes
@@ -69,6 +69,6 @@ app.use('/teacher', teacherRoutes)
 
 
 // for debugging
-server.listen(port, () => {
-    console.log(`Listening on port number`);
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 })
