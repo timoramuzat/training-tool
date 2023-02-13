ebb09311afe0e7a2d00f0a2d1750fe6789d3a384
Timo <70514146+timoramuzat@users.noreply.github.com>
2023-02-14 00:59:49 +0100

    commit firstfsdf

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname));
 
 const server = http.createServer(app);
 
