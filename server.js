46d33a9869c723fd6a0d021ae2e9a031f6051b46
Timo <70514146+timoramuzat@users.noreply.github.com>
2023-02-14 01:08:06 +0100

    commit firstfsdf

--- server.js
+++ server.js
@@ -4,8 +4,7 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT
 
-app.use(express.static(__dirname));
-
+app.use(express.static(__dirname + '/dist'));
+app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 const server = http.createServer(app);
-
 server.listen(port, () => console.log(`App running on port ${port}`));
