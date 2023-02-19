3dac40e9ea156c8efd0f4bca9146a26a4cef832a
SPersim <jespersimon@live.nl>
2023-02-19 22:43:26 +0100

    Update teacher.js

--- api/routes/teacher.js
+++ api/routes/teacher.js
@@ -11,6 +11,7 @@ router.get('/gethomequiz',teacherController.verifyToken,teacherController.getHom
 router.get('/seestudent',teacherController.verifyToken,teacherController.seeStudent)
 router.delete('/deletequiz/:id',teacherController.verifyToken,teacherController.deleteQuiz)
 router.post('/uploadquiz',teacherController.verifyToken,teacherController.uploadQuiz)
+router.post('/downloadquiz',teacherController.verifyToken,teacherController.downloadQuiz)
 router.post('/addquestion',teacherController.verifyToken,teacherController.addQuestion)
 router.get('/getallquestion/:id',teacherController.verifyToken,teacherController.getAllQuestion)
 router.delete('/deletequestion/:id',teacherController.verifyToken,teacherController.deleteQuestion)
