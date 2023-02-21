87f3f6e04de7fb9a46e3f264f78694e872010810
Timo <70514146+timoramuzat@users.noreply.github.com>
2023-02-21 17:01:30 +0100

    new files layout

---
+++ src/app/student/test/test.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { StudentService } from 'src/app/services/student.service';
+import { WebsocketService } from 'src/app/services/websocket.service';
+
+@Component({
+  selector: 'app-studenttest',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent implements OnInit {
+
+  allquiz: any;
+  public loading: any = true;
+  public empty: any = true;
+  constructor(private studentService: StudentService, private router: Router, private webSocketService: WebsocketService) { }
+
+  ngOnInit(): void {
+    this.loading = true
+    this.empty = true
+    this.webSocketService.listen('quizcrud').subscribe(
+      (data) => {
+        this.getdata()
+      }
+    )
+    this.getdata();
+  }
+
+  getdata() {
+    this.studentService.getAllTestQuiz()
+      .subscribe(
+        data => {
+          if (data['quiz']) {
+            this.loading = false
+            this.allquiz = data['quiz']
+            // console.log(this.allquiz.length);
+            if (!this.allquiz.length) {
+              this.empty = true;
+            }
+            else {
+              this.empty = false;
+            }
+          }
+        },
+        error => {
+          this.router.navigate(['/error']);
+        })
+  }
+
+  playquiz(item) {
+    this.studentService.setQuizId(item._id);
+    this.studentService.setQuizName(item.quizname);
+    this.router.navigate(['/student/playquiz']);
+  }
+}
